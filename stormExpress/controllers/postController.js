eb4238e87b9b51a1f493e4229fc9df253166323f
Oleksandr Nykytin <keeper@ninydev.com>
2023-05-10 20:37:24 +0300

    First Controller

--- stormExpress/controllers/postController.js
+++ stormExpress/controllers/postController.js
@@ -23,7 +23,21 @@ exports.getPost = function (request, response) {
 
 exports.createPost = function (request, response) {
     let newPost = request.body
-    console.log(request.body)
+    // console.log(request.body)
     posts.push(newPost)
-    response.status(201).json(newPost)
+    response.status(201).send(newPost)
+}
+
+exports.updatePost = function (request, response) {
+    let postId = request.params.postId
+    let newPost = request.body
+    posts[postId] = newPost
+    response.status(204).send(newPost)
+}
+
+
+exports.deletePost = function (request, response) {
+    let postId = request.params.postId
+    posts.splice(postId, 1)
+    response.status(204).send()
 }
--- stormExpress/routes/posts.js
+++ stormExpress/routes/posts.js
@@ -6,5 +6,7 @@ let postController = require("../controllers/postController")
 router.get('/', postController.getPosts)
 router.get('/:postId', postController.getPost)
 router.post('/', postController.createPost)
+router.put('/:postId', postController.updatePost)
+router.delete('/:postId', postController.deletePost)
 
 module.exports = router;
