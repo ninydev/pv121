8d8caefc533bb4b2d17fab2fe0e41e75f6149a2a
Oleksandr Nykytin <keeper@ninydev.com>
2023-05-13 20:45:18 +0300

    Login Form

---
+++ full_stack/front_vue/src/components/MyAuth/RegisterForm.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import {toast} from "vue3-toastify";
+import {ref} from "vue";
+
+const email = ref("test@test.com");
+
+const password = ref('')
+const confirmPassword = ref('')
+
+const doSend = () =>{
+    if(password.value !== confirmPassword.value) {
+        toast.error("Ваши пароли не совпадают")
+        return
+    }
+    toast.info("Send")
+}
+</script>
+
+<template>
+  <input type="email" name="email" v-model="email"> {{ email }}<br>
+  <input type="password" name="password" v-model="password"><br>
+  <input type="password" name="confirmPassword" v-model="confirmPassword"><br>
+  <input type="button" value="send" @click="doSend">
+</template>
+
+<style scoped>
+
+</style>
--- full_stack/front_vue/src/views/HomeView.vue
+++ full_stack/front_vue/src/views/HomeView.vue
@@ -3,10 +3,11 @@
 import ListAreas from "@/components/NovaPoshta/Areas/ListAreas.vue";
 import PrivatBackExchange from "@/components/PrivatBank/PrivatBackExchange.vue";
 import ExchangeApiList from "@/components/ExchangeApi/ExchangeApiList.vue";
+import RegisterForm from "@/components/MyAuth/RegisterForm.vue";
 </script>
 
 <template>
   <main>
-    <ExchangeApiList></ExchangeApiList>
+    <RegisterForm></RegisterForm>
   </main>
 </template>
