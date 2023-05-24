8fa8499a3265c735a531270727be7cb978c415d1
Oleksandr Nykytin <keeper@ninydev.com>
2023-05-24 19:18:45 +0300

    DB Container

---
+++ full_stack/node_api_auth/db/mySql.js
@@ -0,0 +1,10 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    dialect: "mysql",
+    host: "db.mysql.main"
+});
+
+export default sequelize
