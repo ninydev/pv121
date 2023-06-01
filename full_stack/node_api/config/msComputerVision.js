83d2c97d09971f8fb624f6236c284ce292e4dec2
Oleksandr Nykytin <keeper@ninydev.com>
2023-06-01 19:20:16 +0300

    Подключаем компьютерное зрение

---
+++ full_stack/node_api/config/msComputerVision.js
@@ -0,0 +1,35 @@
+const msRestAzure = require('ms-rest-azure');
+const ComputerVision = require('azure-cognitiveservices-computervision');
+
+// Замените значения переменных соответствующими ключами и конечной точкой вашего ресурса Computer Vision
+const subscriptionKey = process.env.AZURE_COMPUTER_VISION_KEY;
+const endpoint = process.env.AZURE_COMPUTER_VISION_ENDPOINT;
+
+
+// Создайте клиент Computer Vision
+const credentials = new msRestAzure.CognitiveServicesCredentials(subscriptionKey);
+const client = new ComputerVision.ComputerVisionClient(credentials, endpoint);
+
+// Функция для отправки фотографии и получения описания
+async function getImageDescription(imageUrl) {
+    // Отправляем запрос на описание изображения
+    const result = await client.describeImage(imageUrl, { language: 'en' });
+    console.log('Computer Vision result')
+    console.log(result)
+    return result;
+}
+
+
+module.exports = getImageDescription
+/*
+// Пример использования
+const imageUrl = 'URL_TO_YOUR_IMAGE';
+
+getImageDescription(imageUrl)
+    .then(description => {
+        console.log('Описание изображения:', description);
+    })
+    .catch(err => {
+        console.error('Ошибка:', err);
+    });
+*/
