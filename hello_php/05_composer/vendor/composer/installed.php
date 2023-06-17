f126c50c5913a6ea865f9a9900afab2ba2fe4c9c
Oleksandr Nykytin <keeper@ninydev.com>
2023-06-17 19:20:25 +0300

    site composer

---
+++ hello_php/05_composer/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'keeper/05_composer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1c4b986eccfb2e7d864a4a1124d024644d676fb8',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'keeper/05_composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1c4b986eccfb2e7d864a4a1124d024644d676fb8',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '312ed95ab03e7f3f0316191e5bfb9f920b705825',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
