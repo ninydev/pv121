43416af2e6996c15ee6fedb73b2df97a06130c60
Oleksandr Nykytin <keeper@ninydev.com>
2023-06-17 19:44:59 +0300

    Contact Form

--- hello_php/05_composer/public/index.php
+++ hello_php/05_composer/public/index.php
@@ -6,5 +6,42 @@
 // Подключу средство для автозагрузки классов
 require_once ("../vendor/autoload.php");
 
+var_dump($_GET);
+
+$smtpPswd = "";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.ukr.net';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'nika_web@ukr.net';                     //SMTP username
+    $mail->Password   = $smtpPswd;                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('nika_web@ukr.net', 'Mailer form PV121');
+    $mail->addAddress('keeper@ninydev.com', 'Oleksandr Nykytin');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'From ' . $_GET['userName'];
+    $mail->Body    = $_GET['userMessage'];
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
 
 
