04af0cecba27080869acf5c391cf23402edb6c13
Oleksandr Nykytin <keeper@ninydev.com>
2023-06-24 17:27:15 +0300

    Migrate

---
+++ hello_laravel/example-app/database/migrations/2023_06_23_174715_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug', 64)->unique()->index();
+            $table->string('title', 64);
+            $table->text('body');
+            $table->string('img_url', 64)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
