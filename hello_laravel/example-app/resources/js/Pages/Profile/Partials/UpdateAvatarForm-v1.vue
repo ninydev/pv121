540e7bb4df079f29b43b72f3f59093597d572f29
Oleksandr Nykytin <keeper@ninydev.com>
2023-07-07 19:54:54 +0300

    Avatar preload

---
+++ hello_laravel/example-app/resources/js/Pages/Profile/Partials/UpdateAvatarForm-v1.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {usePage} from "@inertiajs/vue3";
+
+const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+const user = usePage().props.auth.user;
+
+</script>
+
+<template>
+
+    <form action="/profile/avatar"
+          enctype="multipart/form-data"
+          method="post">
+
+        <input type="file" name="avatar">
+        <!-- добавляем ключ защиты -->
+        <input type="hidden" name="_token" v-model=csrfToken>
+        <input type="submit">
+    </form>
+
+    <h3>Old Avatar</h3>
+    <img :src="user.avatar" v-if="user.avatar" width="100" />
+
+</template>
+
+<style scoped>
+
+</style>
