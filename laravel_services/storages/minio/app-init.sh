4f3826b4c7962efc30a5359d44776f8c6e506e04
Oleksandr Nykytin <keeper@ninydev.com>
2023-07-15 16:51:50 +0300

    last

--- laravel_services/storages/minio/app-init.sh
+++ laravel_services/storages/minio/app-init.sh
@@ -1,16 +1,19 @@
 ##!/bin/bash
 #
 ## Создание хранилища
-##mc alias set storage http://minio:9000 root password
-## mc config host add storage http://minio:9000 root password
-#mc config host add storage_minio http://storage_minio:9000 TGtZfe96wQPIx0dY kD9IjnOpgdG0l33JSMGhLWmPacNIxw7y
+mc alias set storage http://localhost:9000 root password
+mc config host add storage http://localhost:9000 root password
+
+
+mc config host add storage http://localhost:9000 TGtZfe96wQPIx0dY kD9IjnOpgdG0l33JSMGhLWmPacNIxw7y
 #
 ## Создание нового пользователя
-#mc admin user add storage_minio webuser kfFh3J2gkD56Hl3f4d7
+mc admin user add storage webuser kfFh3J2gkD56Hl3f4d7
 #
 ## Добавление пользователю политики доступа
-#mc admin policy set storage_minio readwrite user=webuser
-#mc admin user key add storage_minio webuser TGtZfe96wQPIx0dY kD9IjnOpgdG0l33JSMGhLWmPacNIxw7y
+mc admin policy atach storage readwrite user=webuser
+mc admin policy create readwrite user=webuser
+mc admin user key add storage webuser TGtZfe96wQPIx0dY kD9IjnOpgdG0l33JSMGhLWmPacNIxw7y
 #
 ##Запуск сервера
 ##mc server --console-address :9009 /data
